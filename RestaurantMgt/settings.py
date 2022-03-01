1043d4e12ec7eb8f86357a5244fc491f17832ca6
Abdellah Kamil <abdellah.kamil2017@gmail.com>
2022-03-01 12:08:36 +0300

    Update setting for Email.

--- RestaurantMgt/settings.py
+++ RestaurantMgt/settings.py
@@ -120,9 +120,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/'static', ]
 STATIC_ROOT = BASE_DIR/'assets'
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+Email_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / 'media'
